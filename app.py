fb103f641725b43b307cafb4a51beb5786efe6cf
opelab <account@office.operation-lab.co.jp>
2016-08-28 03:13:57 +0000

    dynamic routing.

--- app.py
+++ app.py
@@ -5,8 +5,14 @@ from bottle import route, run
 
 
 @route('/hello/')
-def hello():
-    return "Hello World"
+@route('/hello/<user>')
+def hello(user="taro"):
+    return "Hello {user}".format(user=user)
+
+
+@route('/date/<month:re:[a-z]+>/<day:int>/<path:path>')
+def date(month, day, path):
+    return "{month}/{day} {path}".format(month=month, day=day, path=path)
 
 
 run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
